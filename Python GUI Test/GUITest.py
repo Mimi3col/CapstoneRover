335edfe6c6783e0d4c47c1c57cd3aeca3dd90d13
ShaneHolmes10 <48338528+ShaneHolmes10@users.noreply.github.com>
2023-02-13 16:51:08 -0500

    GUI edited to have value checking cababilities. Next we'll incorporate serial Com in the GUI.

--- Python GUI Test/GUITest.py
+++ Python GUI Test/GUITest.py
@@ -1,38 +1,68 @@
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLineEdit, QFrame, QGroupBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLineEdit, QFrame, QGroupBox, QLabel
+
 
 
 
 class QMotorInput(QGroupBox):
 
-    _value = 0.0
+    _motAddr = None
 
-    def __init__(self, startVal=0.0, widgetName="Motor"):
+    _QLineEditMotor = None
+    _QButtonMotor = None
+
+    def __init__(self, motAddr, startVal=0, widgetName="Motor"):
 
         super().__init__(widgetName)
 
-        _value = startVal
+        self._motAddr = motAddr
 
         self.setFixedSize(150, 100)
 
         layoutMI = QGridLayout()
 
-        QLineEditMotor = QLineEdit()
-        QLineEditMotor.setFixedSize(50, 20)
-        QLineEditMotor.setStyleSheet("background-color: white;")
+        self._QLineEditMotor = QLineEdit()
+        self._QLineEditMotor.setText(str(startVal))
+        self._QLineEditMotor.setFixedSize(50, 20)
+        self._QLineEditMotor.setStyleSheet("background-color: white;")
 
-        QButtonMotor = QPushButton('Send')
-        QButtonMotor.setFixedSize(50, 50)
-        QButtonMotor.setStyleSheet("background-color: white;")
+        self._QButtonMotor = QPushButton('Send')
+        self._QButtonMotor.setFixedSize(50, 50)
+        self._QButtonMotor.setStyleSheet("background-color: white;")
+        self._QButtonMotor.clicked.connect(self.QButtonMotor_func)
 
-        layoutMI.addWidget(QLineEditMotor, 0, 0)
-        layoutMI.addWidget(QButtonMotor, 0, 1)
+        layoutMI.addWidget(self._QLineEditMotor, 0, 0)
+        layoutMI.addWidget(self._QButtonMotor, 0, 1)
 
         self.setStyleSheet("background-color: grey;")
 
         self.setLayout(layoutMI)
 
+    def QButtonMotor_func(self):
 
+        val = int(self.get_QLineEditMotor_text())
+        if(not(val >= 0 and val <= 63)):
+            self._QLineEditMotor.setText(str(32))
+
+        print(self._motAddr,",", self.get_QLineEditMotor_text())
+
+    def get_QLineEditMotor_text(self):
+        return self._QLineEditMotor.text()
+
+    def set_QLineEditMotor_text(self, msg):
+        return self._QLineEditMotor.setText(msg)
+    
+def QButtonStop_func():
+    QMotorInputTL.set_QLineEditMotor_text(str(32))
+    QMotorInputTR.set_QLineEditMotor_text(str(32))
+    QMotorInputBL.set_QLineEditMotor_text(str(32))
+    QMotorInputBR.set_QLineEditMotor_text(str(32))
+
+    QMotorInputTL.QButtonMotor_func()
+    QMotorInputTR.QButtonMotor_func()
+    QMotorInputBL.QButtonMotor_func()
+    QMotorInputBR.QButtonMotor_func()
+    
 
 app = QApplication([])
 window = QWidget()
@@ -44,19 +74,30 @@ layout = QGridLayout()
 QButtonStop = QPushButton('STOP!')
 QButtonStop.setFixedSize(100, 100)
 QButtonStop.setStyleSheet("background-color: red;")
+QButtonStop.clicked.connect(QButtonStop_func)
 
 
-QMotorInputTL = QMotorInput(0.0, "Motor Top Left")
-QMotorInputTR = QMotorInput(0.0, "Motor Top Right")
-QMotorInputBL = QMotorInput(0.0, "Motor Bottom Left")
-QMotorInputBR = QMotorInput(0.0, "Motor Bottom Right")
+QInstruction = QLabel("The value should range from 0 to 63. Look at the README or code comments to know more.")
+QInstruction.setFixedSize(100, 100)
+QInstruction.setWordWrap(True)
+
+QMotorInputTL = QMotorInput(0, widgetName="Motor Top Left")
+QMotorInputTR = QMotorInput(1, widgetName="Motor Top Right")
+QMotorInputBL = QMotorInput(2, widgetName="Motor Bottom Left")
+QMotorInputBR = QMotorInput(3, widgetName="Motor Bottom Right")
+
+
+
 
 layout.addWidget(QButtonStop, 0, 1)
-layout.addWidget(QMotorInputTL, 1, 0)
-layout.addWidget(QMotorInputTR, 1, 2)
+layout.addWidget(QInstruction, 1, 1)
 
-layout.addWidget(QMotorInputBL, 2, 0)
-layout.addWidget(QMotorInputBR, 2, 2)
+
+layout.addWidget(QMotorInputTL, 2, 0)
+layout.addWidget(QMotorInputTR, 2, 2)
+
+layout.addWidget(QMotorInputBL, 3, 0)
+layout.addWidget(QMotorInputBR, 3, 2)
 
 
 window.setLayout(layout)
