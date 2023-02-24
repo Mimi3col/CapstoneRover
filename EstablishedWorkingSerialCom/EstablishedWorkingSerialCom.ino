74ea390b9fd2c9f4b620c4c84ce6c885dd16e686
ShaneHolmes10 <48338528+ShaneHolmes10@users.noreply.github.com>
2023-02-24 14:44:13 -0500

    This is a version of establishedworkingserialcom has actual implimentation for throttle value discrimination

--- EstablishedWorkingSerialCom/EstablishedWorkingSerialCom.ino
+++ EstablishedWorkingSerialCom/EstablishedWorkingSerialCom.ino
@@ -1,9 +1,13 @@
-#define LEDPin1 6
-#define LEDPin2 7
-#define LEDPin3 8
-#define LEDPin4 9
+#define LEDPin1 12
+#define LEDPin2 11
+#define LEDPin3 10
+#define LEDPin4 8
 
-#define MotValPin 3
+#define MotValBLPin 3
+#define MotValBRPin 5
+#define MotValTLPin 6
+#define MotValTRPin 9
+#define conPin 4 
 
 int msg;
 int motAddr;
@@ -15,6 +19,7 @@ int get_motor_address(int msg) {
 }
 
 int get_motor_value(int msg) {
+  
   return msg & 0b00111111;
 }
 
@@ -27,10 +32,23 @@ void setup() {
   pinMode(LEDPin3, OUTPUT);
   pinMode(LEDPin4, OUTPUT);
 
+  pinMode(MotValBLPin, OUTPUT);
+  pinMode(MotValBRPin, OUTPUT);
+  pinMode(MotValTLPin, OUTPUT);
+  pinMode(MotValTRPin, OUTPUT);
+
+
+
   digitalWrite(LEDPin1, LOW);
   digitalWrite(LEDPin2, LOW);
   digitalWrite(LEDPin3, LOW);
   digitalWrite(LEDPin4, LOW);
+
+
+  analogWrite(MotValBLPin, 0);
+  analogWrite(MotValBRPin, 0);
+  analogWrite(MotValTLPin, 0);
+  analogWrite(MotValTRPin, 0);
   
 }
 
@@ -45,12 +63,14 @@ void loop() {
 
     if(motAddr == 0) { // 00
       digitalWrite(LEDPin1, HIGH);
+      analogWrite(MotValBLPin, (float(motVal)/63)*255);
     } else {
       digitalWrite(LEDPin1, LOW);
     }
     
     if(motAddr == 1) { // 01
       digitalWrite(LEDPin2, HIGH);
+      analogWrite(MotValBRPin, (float(motVal)/63)*255);
     } else {
       digitalWrite(LEDPin2, LOW);
     }
@@ -67,13 +87,8 @@ void loop() {
       digitalWrite(LEDPin4, LOW);
     }
 
-    analogWrite(MotValPin, (float(motVal)/63)*255);
-
+    
+    
     
   }
 }
-
-
-
-
-    
